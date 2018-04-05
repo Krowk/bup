d84c81b0e1d74530f7958df62b4e517eb1485bff
Lockl34d <thomas.bidanchon@etu.univ-tours.fr>
2018-04-05 23:10:47 +0200

    Servlet

---
+++ src/servlet/UserServlet.java
@@ -0,0 +1,166 @@
+package servlet;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.DigestException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.nio.charset.StandardCharsets;
+import java.security.DigestException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String pseudo = request.getParameter("pseudo");
+		String mail = request.getParameter("mail");
+		String mdp = request.getParameter("password");
+		String date = request.getParameter("date");
+		
+		try {
+			pseudo = decrypt(pseudo);
+			mail = decrypt(mail);
+			mdp = decrypt(mdp);
+			date = decrypt(date);
+			
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		doGet(request, response);
+	}
+	
+	
+	
+	public static byte[][] GenerateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password,
+			MessageDigest md) {
+
+		int digestLength = md.getDigestLength();
+		int requiredLength = (keyLength + ivLength + digestLength - 1) / digestLength * digestLength;
+		byte[] generatedData = new byte[requiredLength];
+		int generatedLength = 0;
+
+		try {
+			md.reset();
+
+			// Repeat process until sufficient data has been generated
+			while (generatedLength < keyLength + ivLength) {
+
+				// Digest data (last digest if available, password data, salt if
+				// available)
+				if (generatedLength > 0)
+					md.update(generatedData, generatedLength - digestLength, digestLength);
+				md.update(password);
+				if (salt != null)
+					md.update(salt, 0, 8);
+				md.digest(generatedData, generatedLength, digestLength);
+
+				// additional rounds
+				for (int i = 1; i < iterations; i++) {
+					md.update(generatedData, generatedLength, digestLength);
+					md.digest(generatedData, generatedLength, digestLength);
+				}
+
+				generatedLength += digestLength;
+			}
+
+			// Copy key and IV into separate byte arrays
+			byte[][] result = new byte[2][];
+			result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
+			if (ivLength > 0)
+				result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
+
+			return result;
+
+		} catch (DigestException e) {
+			throw new RuntimeException(e);
+
+		} finally {
+			// Clean out temporary data
+			Arrays.fill(generatedData, (byte) 0);
+		}
+	}
+
+	public static String decrypt(String n) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+
+		String pseudo = n;
+
+		String secret = "dbrCUoc4z9EFJTLBSsZtQw==";
+
+		byte[] cipherData = Base64.getDecoder().decode(pseudo);
+		byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
+
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		final byte[][] keyAndIV = GenerateKeyAndIV(32, 16, 1, saltData, secret.getBytes(StandardCharsets.UTF_8), md5);
+		SecretKeySpec key = new SecretKeySpec(keyAndIV[0], "AES");
+		IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
+
+		byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
+		Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aesCBC.init(Cipher.DECRYPT_MODE, key, iv);
+
+		byte[] decryptedData = aesCBC.doFinal(encrypted);
+		String decryptedText = new String(decryptedData, StandardCharsets.UTF_8);
+
+		return decryptedText;
+
+	}
+
+}
